932cc8651aca0616462e1e5009ebb09ddd78eabd
jafmoralesss <jafetmoralesmartinez@gmail.com>
2025-10-30 22:50:32 -0600

    v3.0.0/Added Offer classes, added mustache and CSS requirements

---
+++ CollectorZone/src/main/java/org/example/Model/Offer.java
@@ -0,0 +1,11 @@
+package org.example.Model;
+
+import java.util.UUID;
+
+public class Offers {
+
+    private String name;
+    private String email;
+    private UUID id;
+    private double price;
+}
